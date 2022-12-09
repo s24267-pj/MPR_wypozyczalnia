b152b814e88d2809eb61a89288fe976566e9ae2d
Damian Ligeza <s24267@pjwstk.edu.pl>
2022-12-09 21:05:00 +0100

    Dodano test dla: Samochód już wynajęty przez kogoś innego

--- src/test/java/com/example/wypozyczalnia/RentalStorageTest.java
+++ src/test/java/com/example/wypozyczalnia/RentalStorageTest.java
@@ -1,4 +1,34 @@
 package com.example.wypozyczalnia;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class RentalStorageTest {
+    private RentalStorage rentalStorage = new RentalStorage();
+
+
+    @Test
+    void test() {
+
+        //GIVEN
+        String vin = "acf";
+
+        //WHEN
+
+
+        List<Rental> rentalList = rentalStorage.getRentalList();
+        boolean wynajety = false;
+        for (Rental rental : rentalList) {
+            if (rental.getCar().getVin().equals(vin)) {
+                wynajety = true;
+                break;
+            }
+        }
+
+        //THEN
+        Assertions.assertFalse(wynajety, "Samochód jest wynajęty");
+    }
+
 }
